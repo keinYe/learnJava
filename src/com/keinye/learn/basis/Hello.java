9990e6dfd15a5e4e1d03b07e55dae49e6085fe09
keinYe <keinye.dev@gmail.com>
2021-01-16 08:35:57 +0800

    first commit

---
+++ src/com/keinye/learn/basis/Hello.java
@@ -0,0 +1,19 @@
+package com.keinye.learn.basis;
+
+/**
+ * 
+ * @author keinYe
+ *
+ */
+// public 访问修饰符，表示是公开的
+// class 用来定义一个类
+// Hello 类名，通常首字母大写
+public class Hello {
+	// main 方法名，通常首字母小谢
+	// void 方法返回值，这里表示无返回值
+	// static 表示该方法为静态方法
+	// args 方法能够接收的参数，字符串数组
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
---
+++ src/com/keinye/learn/basis/Variable.java
@@ -0,0 +1,61 @@
+package com.keinye.learn.basis;
+
+public class Variable {
+	public static void main(String[] args) {
+		//变量先定义后使用
+		int x = 100;
+		System.out.println(x);
+		
+		int n = x;
+		System.out.println(n);
+		
+		x += 100;
+		System.out.println("x = " + x);
+		System.out.println("n = " + n);
+	}
+	
+	/**
+	 * Java 中基本数据类型分为以下几种：
+	 * 整数类型：byte short int long
+	 * 浮点数类型：float double
+	 * 字符类型：char
+	 * 布尔类型：boolean
+	 */
+	
+	/**
+	 * 整数类型中占用的内存空间：
+	 * byte - 1 字节
+	 * short - 2 字节
+	 * int - 4 字节
+	 * long - 8 字节
+	 */
+	
+	/**
+	 * 浮点数类型中 float 的定义要在数值后面加上一个 f，例如 float pi = 3.14f。
+	 */
+	
+	/**
+	 * 布尔类型boolean 只有 false 和 true 两个值。
+	 */
+	
+	/**
+	 * 字符类型占有 2 个字节，标识 ASCII 字符或 Unicode 字符。
+	 */
+
+	/**
+	 * 在 Java 中除了整数类型、浮点数类型、字符类型、布尔类型以外，其他的均为引用类型
+	 */
+	
+	/**
+	 * 在 Java 中使用 final 修饰符修饰的变量即为常量。
+	 */
+	
+	/**
+	 * 使用 var 关键字可以在定义变量时省略变量类型，由编译器自动推断出变量的类型。
+	 */
+	
+	/**
+	 * Java 中变量的作用域通常是从变量定义处到作用域结尾
+	 * Java 中的作用域指两个花括号之间的内容
+	 */
+}
